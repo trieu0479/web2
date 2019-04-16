29a4eab8d018b24a3c01d1264a66536bd9f805c6
Nguyen Trieu <trieu0479@gmail.com>
2019-04-16 21:57:11 +0700

    chia footer header tao cau truc thu muc

--- mvc/app.js
+++ mvc/app.js
@@ -4,8 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
+
+var hbs = require('hbs');
+var fs = require('fs');
 
 var app = express();
 
@@ -13,14 +16,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+let header = fs.readFileSync(path.join(__dirname,"views","user","template","header.hbs"), 'utf-8');
+let footer = fs.readFileSync(path.join(__dirname,"views","user","template","footer.hbs"), 'utf-8');
+
+hbs.registerPartial('userHeader',  header);
+hbs.registerPartial('userFooter',  footer);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
