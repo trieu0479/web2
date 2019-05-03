1678b9f89919c1b89ada423c0be01e6c839f0a05
= <=>
2019-05-03 16:21:32 +0700

    ...

--- mvc/models/dashboard.model.js
+++ mvc/models/dashboard.model.js
@@ -12,10 +12,14 @@ module.exports = {
         let sql = 'SELECT * FROM `baiviet` join `danhmuc` on baiviet.IDDanhMuc =  danhmuc.IDDanhMuc WHERE YEARWEEK(`NgayDang`, 1) = YEARWEEK(CURDATE(), 1) ORDER BY SoLuotXem DESC LIMIT 5';
         return db.load(sql);
     },
-    lay10baimoinhat: () =>{
+    lay10baixemnhieu: () =>{
         let sql = 'select * from `baiviet` join `danhmuc` on baiviet.IDDanhMuc =  danhmuc.IDDanhMuc order by SoLuotXem desc limit 0,10';
         return db.load(sql);
     },
-    
-
+    lay10baimoinhat:() =>{
+        return db.load('select * from `baiviet` join `danhmuc` on baiviet.IDDanhMuc =  danhmuc.IDDanhMuc order by NgayDang desc limit 0,10')
+    },
+    top10chuyenmuc: ()=>{
+        return db.load('SELECT * FROM `baiviet` join `danhmuc` on baiviet.IDDanhMuc =  danhmuc.IDDanhMuc GROUP BY IDChuyenMuc ORDER BY NgayDang DESC limit 0,10');
+    }
 }
--- mvc/routes/user.js
+++ mvc/routes/user.js
@@ -8,8 +8,10 @@ var router = express.Router();
 router.get('/', async function (req, res, next) {
     let data = {};
     data.baiVietNoiBat = await dashboardModel.get3PostForDashborad();
-    data.lay10baimoinhat = await dashboardModel.lay10baimoinhat();
+    data.lay10baixemnhieu = await dashboardModel.lay10baixemnhieu();
     data.laymenu = await dashboardModel.laymenu();
+    data.lay10baimoinhat = await dashboardModel.lay10baimoinhat();
+    data.top10chuyenmuc = await dashboardModel.top10chuyenmuc();
     res.render('user/index', data);
 });
 
